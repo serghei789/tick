65d5ceb2632d5e5a1d3dab65d63e61d954a9ad9b
Serghei <78849747+serghei789@users.noreply.github.com>
2024-06-16 18:06:44 +0300

    база

---
+++ model/passwords.py
@@ -0,0 +1,6 @@
+#За паролями писать https://t.me/mgarbuzenko
+dbLocal  = 'mysql+pymysql://root:root@localhost:3306/ships_new'
+dbServer = 'mysql+pymysql://u2671630_default:ПАРОЛЬ@vip172.hosting.reg.ru:3306/u2671630_default'
+dbBackup = 'mysql+pymysql://u2671630_default:ПАРОЛЬ@vip172.hosting.reg.ru:3306/u2671630_backup'
+api_key = 'КЛЮЧ_СЕКРЕТНЫЙ'
+secret_key = 'КЛЮЧ СЕКРЕТНЫЙ'
